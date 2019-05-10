10a2525b2c828694a5ae7f79ddcc460dde0852db
Itsuki Murayama <s1711472@s.tsukuba.ac.jp>
2019-05-10 17:05:45 +0900

    DBの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-Eatery.create(name: "七福軒", zip;"305-0005", 
+Eatery.create(name: "七福軒", zip:"305-0005", 
 address:"茨城県つくば市天久保1丁目6-14", tel:"029-893-4799")
-Eatery.create(name: "天地", zip;"305-0005", 
+Eatery.create(name: "天地", zip:"305-0005", 
 address:"茨城県つくば市天久保2丁目6-26", tel:"029-860-7710")
-Eatery.create(name: "清六屋", zip;"305-0005", 
+Eatery.create(name: "清六屋", zip:"305-0005", 
 address:"茨城県つくば市天久保3丁目4-8", tel:"029-875-9769")
-Eatery.create(name: "龍郎", zip;"305-0031", 
+Eatery.create(name: "龍郎", zip:"305-0031", 
 address:"茨城県つくば市吾妻3丁目8-1", tel:"029-859-3311")
-Eatery.create(name: "鶏々", zip;"305-0005", 
+Eatery.create(name: "鶏々", zip:"305-0005", 
 address:"茨城県つくば市天久保1丁目6-14", tel:"029-860-6025")
