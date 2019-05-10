505577e60ba1b03482649d431c1286d4abdf2b83
Itsuki Murayama <s1711472@s.tsukuba.ac.jp>
2019-05-10 15:50:56 +0900

    選択肢の追加

--- app/controllers/lunch_controller.rb
+++ app/controllers/lunch_controller.rb
@@ -1,6 +1,6 @@
 class LunchController < ApplicationController
     def go
-        eataries = ['清六屋','龍郎','七福軒','天地','どぶ']
+        eataries = ['清六屋','龍郎','七福軒','天地','番長']
         @recommend = eataries.sample
     end
 end
