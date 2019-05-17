8afc13c2a82bb378ba0d50424c247aead52e55c3
Itsuki Murayama <s1711472@s.tsukuba.ac.jp>
2019-05-17 17:30:48 +0900

    ユーザ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'eateries/new'
   get 'lunch/go'
   root'lunch#go'
   post 'eateries/create'
+  get '/login', to:'sessions#new'
+  post 'sessions/create'
 end
